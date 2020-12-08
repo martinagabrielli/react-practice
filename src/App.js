6558dbdedab3684cc3bcc4ea25debe190414ed64
martinagabrielli <martina.gabrielli@yahoo.it>
2020-12-08 14:25:34 +0000

    practice with useState hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [count, setCount] = useState(4)
+
+  function decrementCount() {
+    setCount(prevCount => prevCount - 1)
+  }
+
+  function incrementCount() {
+    setCount(prevCount => prevCount + 1)
+  }
+
   return (
     <div className="App">
-      
+      <button onClick={decrementCount}>-</button>
+      <span>{count}</span>
+      <button onClick={incrementCount}>+</button>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
